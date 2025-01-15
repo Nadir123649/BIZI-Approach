d8cf8540ed68abdbdc25b8556715e20fe4411fa0
Nadir123649 <nadir.ali@novatoresols.com>
2025-01-15 22:15:42 +0500

    add canges

--- src/components/CarsSection.jsx
+++ src/components/CarsSection.jsx
@@ -7,7 +7,7 @@ import { cardData } from '../constants';
 const CarsSection = () => {
     return (
         <section className='px-[40px] py-[100px]'>
-            <div>
+            <div className='max-w-[1400px] w-full mx-auto'>
                 <TitleComponent title="Type of cares" className="flex items-center justify-start gap-[30px] mt-[0px] ml-[0px]" />
                 <Row className='px-[40px] flex-margin'>
                     {cardData.map((card) => (
--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -2,36 +2,42 @@ import React from 'react';
 
 const MainCard = ({ card }) => {
     return (
-        <div className='card-bordered'
+        <div className='card-bordered relative'
             style={{
                 backgroundImage: `url(${card.backgroundImage})`,
-                // backgroundSize: 'cover',
+                backgroundSize: 'contain',
+                backgroundPosition: 'center',
+                backgroundOrigin: 'border-box',
+                backgroundClip: 'border-box',
+                borderRadius: '20px',
                 backgroundRepeat: 'no-repeat',
             }}>
-            <img src={card.logo} alt="logo-card" />
-            <div>
-                <div>
-                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF]'>ACCELERATION</h3>
-                    <img src={card.imageOne} alt="one-progress" />
-                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic'>{card.valueone}</p>
+            <div className='relative top-[-26px]'>
+                <img src={card.logo} alt="logo-card" className='mx-auto block ' />
+            </div>
+            <div className='max-w-[255px] w-full mx-auto pr-[0px]'>
+                <div className='h-[70px]'>
+                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF] mb-[2px]'>ACCELERATION</h3>
+                    <img src={card.imageOne} alt="one-progress" className='w-full' />
+                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic mt-[2px] mb-[2px]'>{card.valueone}</p>
                 </div>
-                <div>
-                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF]'>max speed</h3>
-                    <img src={card.imageTwo} alt="two-progress"  />
-                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic'>{card.valuetwo}</p>
+                <div className='h-[70px]'>
+                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF] mb-[2px]'>max speed</h3>
+                    <img src={card.imageTwo} alt="two-progress" className='w-full' />
+                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic mt-[2px] mb-[2px]'>{card.valuetwo}</p>
                 </div>
-                <div>
-                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF]'>HANDLING</h3>
-                    <img src={card.imageThree} alt="three-progress"  />
-                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic'>{card.valuethree}</p>
+                <div className='h-[70px]'>
+                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF] mb-[2px]'>HANDLING</h3>
+                    <img src={card.imageThree} alt="three-progress" className='w-full' />
+                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic mt-[2px] mb-[2px]'>{card.valuethree}</p>
                 </div>
-                <div>
-                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF]'>BRAKING</h3>
-                    <img src={card.imageFour} alt="four-progress" />
-                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic'>{card.valuefour}</p>
+                <div className='h-[70px]'>
+                    <h3 className='leading-normal not-italic text-[16px] font-[400] text-[#FFFFFF] mb-[2px]'>BRAKING</h3>
+                    <img src={card.imageFour} alt="four-progress" className='w-full' />
+                    <p className='text-right text-[10px] font-[400] text-[#FFFFFF] leading-normal not-italic mt-[2px] mb-[2px]'>{card.valuefour}</p>
                 </div>
             </div>
-            <div className='upgrade-box'>
+            <div className='upgrade-box relative top-[-10px] ml-[20px]'>
                 <img src={card.upgrade} alt="upgrade" />
             </div>
         </div>
